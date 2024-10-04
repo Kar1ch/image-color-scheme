0a12780c6d69ed1cea4e1074dcdb88d6df88304a
Kar1ch <shibanovmd@gmail.com>
2024-10-04 23:05:26 +0000

    научился красить фон в главный цвет изображения

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
+MAX_COLOR = 128
+
 import sys
+import os
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+from materialyoucolor.quantize import QuantizeCelebi, StbLoadImage
+from materialyoucolor.score.score import Score
 from PIL import Image, ImageTk
 
-root = Tk()
 
-button_quit = Button(root, text='Exit Program', command=root.quit)
-button_quit.pack()
+if __name__ == "__main__":
+    root = Tk()
 
-
-my_label = Label()
-my_label.pack()
+    button_quit = Button(root, text='Exit Program', command=root.quit, borderwidth=0)
+    button_quit.pack()
 
 
-def myClick():
-    link = askopenfilename()
-    my_img = ImageTk.PhotoImage(Image.open(link))
-    my_label.configure(image=my_img)
-    my_label.image = my_img
+    my_label = Label(borderwidth = 0)
+    my_label.pack()
 
 
-myButton = Button(root, text='Scan Part Number', command=myClick,
-                  bg='pink', fg='white')
-myButton.pack()
+    def myClick():
+        link = askopenfilename()
+        imageTk = ImageTk.PhotoImage(Image.open(link))
+        image = Image.open(link)
+        pixel_len = image.width * image.height
+        image_data = image.getdata()
+        pixel_array = [image_data[_] for _ in range(0, pixel_len)]
+        pixel_array = StbLoadImage(link)
+        
+        colors = QuantizeCelebi(pixel_array, MAX_COLOR)
+        #print(colors)
+
+        selected = Score.score(colors)
+        print(selected)
+        
+        bgcolor = "#" + hex(selected[0])[4::]
+        root.configure(background = bgcolor)
+
+        #print(pixel_len)
+        #print(image_data)
+        my_label.configure(image=imageTk)
+        my_label.image = imageTk
 
 
+    myButton = Button(root, text='Scan Part Number', command=myClick,
+                  bg='#ffaaaa', fg='#ffffff', borderwidth=0)
+    myButton.pack()
 
-root.mainloop()
+    root.mainloop()
