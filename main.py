d2573d073efde02868c294c78a87a57fc1fa7428
Kar1ch <shibanovmd@gmail.com>
2024-10-04 22:03:12 +0000

    показываю выбранную картинку

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import sys
-import tkinter as tk
+from tkinter import *
+from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 
-root = tk.Tk()
+root = Tk()
+
+button_quit = Button(root, text='Exit Program', command=root.quit)
+button_quit.pack()
+
+
+my_label = Label()
+my_label.pack()
+
+
+def myClick():
+    link = askopenfilename()
+    my_img = ImageTk.PhotoImage(Image.open(link))
+    my_label.configure(image=my_img)
+    my_label.image = my_img
+
+
+myButton = Button(root, text='Scan Part Number', command=myClick,
+                  bg='pink', fg='white')
+myButton.pack()
+
+
+
 root.mainloop()
